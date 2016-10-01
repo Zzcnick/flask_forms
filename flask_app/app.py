2f9755e44840ae6899cb4050c2b34b44a906eb81
Zzcnick <zzhen@stuy.edu>
2016-10-01 15:19:01 -0400

    Complete basic registration form

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,31 +1,77 @@
 # Flask App for Forms
 
 from flask import Flask, render_template, redirect, request
+from utils import authenticate
 app = Flask(__name__)
 
 # Website Routes
 # ===============================================================
 @app.route("/", methods=["GET"])
 def index():
-    print request.form, "\n===================================\n"
+    print request.form, "\n===================================\n" # Debugging
     return render_template("form.html",
-                           title="A Form")
+                           title="Login")
 
-@app.route("/authenticate/", methods=["POST"]) 
+@app.route("/login/", methods=["POST", "GET"]) 
 def auth():
-    if verify(request.form) > 0:
-        return render_template("auth.html",
-                               status="Success!")
-    return render_template("auth.html",
-                           status="Failure!")
-
-# Login Mechanism - To Be Replaced
+    print request.form, "\n===================================\n" # Debugging
+    try:
+        action = request.form["action"]
+    except:
+        return render_template("form.html",
+                               title="Login")
+    if action == "Login":
+        return login(request.form["username"],request.form["password"])
+    if action == "Register":
+        return register(request.form["username"],request.form["password"])
+    
+# Formatting - Should this be in authenticate? 
 # ===============================================================
-def verify(form):
-    if (form["username"] == "Foo" and
-        form["password"] == "bar"):
-        return 1
-    return -1
+def login(username,password):
+    logstatus = authenticate.verify(username,password)
+    if logstatus > 0:
+        return render_template("form.html",
+                               title="Login",
+                               status="Login Success!")
+    elif logstatus == -1:
+        return render_template("form.html",
+                               title="Login",
+                               status="Login Failed: Unknown Error!")
+    elif logstatus == -2:
+        return render_template("form.html",
+                               title="Login",
+                               status="Login Failed: User does not exist.")
+
+def register(username,password):
+    regstatus = authenticate.register(username,password)
+    if regstatus > 0:
+        return render_template("form.html",
+                               title="Login",
+                               status="Registration Success!")                    
+    elif regstatus == -1:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Username already exists.")                    
+    elif regstatus == -2:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Invalid username.")
+    elif regstatus == -3:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Password too short.")
+    elif regstatus == -4:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Chill with your password length, eh?")
+    elif regstatus == -5:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Username too short.")
+    elif regstatus == -6:
+        return render_template("form.html", 
+                               title="Login",
+                               status="Registration Failed: Surely you can choose a shorter name to identify yourself.")
 
 # Prevents following code from running unless it is standalone.
 # ===============================================================
---
+++ flask_app/utils/authenticate.py
@@ -0,0 +1,83 @@
+# Authentification Related Stuff
+
+import hashlib
+
+# Read and Write to Usernames and Passwords
+# ===============================================================
+def read_usernames():
+    f = open("data/username.csv", "r")
+    raw = f.read().strip().split("\n")
+    f.close()
+    for i in raw: print i # Debugging
+    return raw
+
+def read_passwords():
+    f = open("data/password.csv", "r")
+    raw = f.read().strip().split("\n")
+    f.close()
+    for i in raw: print i # Debugging
+    return raw
+
+def write_data(username, password):
+    u = open("data/username.csv", "r")
+    p = open("data/password.csv", "r")
+    uraw = u.read().strip()
+    praw = p.read().strip()
+    u.close()
+    p.close()
+    uraw += "\n" + username
+    praw += "\n" + encrypt(password)
+    u = open("data/username.csv", "w")
+    p = open("data/password.csv", "w")
+    u.write(uraw)
+    p.write(praw)
+    u.close()
+    p.close()
+
+# Encryption
+# ===============================================================
+def encrypt(password):
+    # We all need more salt in our lives
+    hashed = password + "733f8d6d68ab4e5bd46a3ab56186311b3bfd3705f2f4029f0f3e06a6e91f03d89b94d1546c669b0aceb10cd5000ca4d0"
+    return hashlib.sha384(hashed).hexdigest()
+
+# Verification
+# ===============================================================
+def verify(username,password):
+    usernames = read_usernames()
+    passwords = read_passwords()
+    try:
+        row = usernames.index(username)
+    except:
+        return -2 
+    if (encrypt(password) == passwords[row]):
+        return 1
+    return -1
+
+def register(username,password):
+    usernames = read_usernames()
+    plen = len(password)
+    ulen = len(username)
+
+    # Possible Errors
+    if username in usernames:
+        return -1 # Username Already Exists
+    for ch in username:
+        c = ord(ch)
+        if (c < 48 or
+            (c > 57 and c < 65) or
+            (c > 90 and c < 97) or
+            c > 122):
+            return -2 # Invalid Username
+    if (ulen < 2):
+        return -5 # Short Username
+    if (ulen > 32):
+        return -6 # Long Username
+    if (plen < 4):
+        return -3 # Short Password
+    if (plen > 64):
+        return -4 # Long Password
+
+    # Success
+    write_data(username,password)
+    return 1
